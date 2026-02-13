8f5819b27a0e47680883fba26e6278a1636baa3d
Gage Johnson <gagejohnson117@gmail.com>
2026-02-12 18:58:44 -0600

    8 Failed tests still have to submit before class

--- patterns/nested.js
+++ patterns/nested.js
@@ -36,7 +36,18 @@ export function makeBoard(rows, cols) {
  * @returns `[]` if `size` is 0 or negative
  */
 export function makeTriangle(size) {
-  // TODO
+  if (typeof size !== "number") return null;
+  if (size <= 0) return [];
+
+  const triangle = [];
+  for (let i = 1; i <= size; i++) {
+    const row = [];
+    for (let j = 0; j < 1; j++) {
+      row.push("-");
+    }
+    triangle.push(row);
+  }
+  return triangle;
 }
 
 /**
@@ -46,5 +57,16 @@ export function makeTriangle(size) {
  * @returns `0` if `letter is not a string
  */
 export function countLetter(words, letter) {
-  // TODO
+  if (typeof letter !== "string") return 0;
+  if (words.length === 0) return 0;
+
+  let count = 0;
+  for (const word of words) {
+    for (const char of word) {
+      if (char.toLowerCase() === letter.toLocaleLowerCase) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
